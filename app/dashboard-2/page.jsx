1493e8880d50393009ef004d8be870122585787e
JiriDavid <jiridavidpromise@gmail.com>
2024-07-19 16:59:39 +0530

    improved dashboard-2 ui

--- app/dashboard-2/page.jsx
+++ app/dashboard-2/page.jsx
@@ -4,7 +4,20 @@ import ChallengeCard from "@/components/DashComponents/ChallengeCard";
 
 export default function DashBoard() {
   return (
-    <div className="md:p-12 ml-64 mt-16 h-screen mr-0">
+    <div className="md:p-12 ml-64 mt-10 h-screen mr-0">
+      <div className="flex overflow-x-scroll h-16 space-x-4 overflow-y-hidden items-center">
+        <button className=" rounded-md px-4 py-2 border h-8 flex justify-center items-center hover:bg-gray-100">All</button>
+        <button className=" rounded-md px-4 py-2 border h-8 flex justify-center items-center hover:bg-gray-100">Frontend</button>
+        <button className=" rounded-md px-4 py-2 border h-8 flex justify-center items-center hover:bg-gray-100">Backend</button>
+        <button className=" rounded-md px-4 py-2 border h-8 flex justify-center items-center hover:bg-gray-100">Fullstack</button>
+        <button className=" rounded-md px-4 py-2 border h-8 flex justify-center items-center hover:bg-gray-100">DevOps</button>
+        <button className=" rounded-md px-4 py-2 border h-8 flex justify-center items-center hover:bg-gray-100">Design</button>
+        <button className=" rounded-md px-2 py-2 border h-8 flex justify-center items-center min-w-[130px] hover:bg-gray-100">Data Science</button>
+        <button className=" rounded-md px-4 py-2 border h-8 flex justify-center items-center hover:bg-gray-100">Mobile</button>
+        <button className=" rounded-md px-4 py-2 border h-8 flex justify-center items-center hover:bg-gray-100">AI/ML</button>
+        <button className=" rounded-md px-2 py-2 border h-8 flex justify-center items-center min-w-[130px] hover:bg-gray-100">Cyber Security</button>
+        <button className=" rounded-md px-2 py-2 border h-8 flex justify-center items-center min-w-[170px] hover:bg-gray-100">Game Development</button>
+      </div>
       <div className="flex justify-end flex-wrap space-x-4 ">
         <ChallengeCard />
         <ChallengeCard />
--- components/DashComponents/LeftSideBar.jsx
+++ components/DashComponents/LeftSideBar.jsx
@@ -8,32 +8,38 @@ import { CiMenuKebab } from "react-icons/ci";
 
 function LeftSideBar() {
   return (
-    <div className="w-72 flex flex-col justify-start items-center px-3 fixed space-y-2 h-[486px] overflow-scroll pt-2 left-0 top-20 z-40">
-      <button className="rounded-lg bg-gray-100 w-full flex px-2 h-12 justify-start items-center space-x-2 ">
-        <BsBrowserSafari className="h-6 w-6" /> <span>Browse</span>
-      </button>
-      <button className="rounded-lg hover:bg-gray-100 w-full flex px-2 h-12 justify-start items-center space-x-2 ">
-        <GrWorkshop className="h-6 w-6" /> <span>Workshops</span>
-      </button>
-      <button className="rounded-lg hover:bg-gray-100 w-full flex px-2 h-12 justify-start items-center space-x-2 ">
-        <MdLeaderboard className="h-6 w-6" /> <span>Leaderboard</span>
-      </button>
-      <button className="rounded-lg hover:bg-gray-100 w-full flex px-2 h-12 justify-start items-center space-x-2">
-        <RiNewsLine className="h-6 w-6" /> <span>NewsLetter</span>
-      </button>
-      <div className="border p-3 rounded-lg mt-4 mb-8">
-        <h2 className="text-lg font-bold py-2">Upgrade to pro</h2>
-        <p className="pb-2 text-sm">
-          Unlock all courses, get access to source code, and more.
-        </p>
-        <button className="bg-green-950 text-white w-full rounded-lg py-2 hover:bg-green-900">
-          Upgrade
+    <div className="w-72 flex flex-col justify-start items-center fixed space-y-2 h-[473px] pt-2 left-0 top-20 z-40 overflow-y-scroll">
+      <div className="flex flex-col space-y-2 w-full pt-2 pb-2 px-3">
+        <button className="rounded-lg bg-gray-100 w-full flex px-2 h-12 justify-start items-center space-x-2 ">
+          <BsBrowserSafari className="h-6 w-6" /> <span>Browse</span>
+        </button>
+        <button className="rounded-lg hover:bg-gray-100 w-full flex px-2 h-12 justify-start items-center space-x-2 ">
+          <GrWorkshop className="h-6 w-6" /> <span>Workshops</span>
+        </button>
+        <button className="rounded-lg hover:bg-gray-100 w-full flex px-2 h-12 justify-start items-center space-x-2 ">
+          <MdLeaderboard className="h-6 w-6" /> <span>Leaderboard</span>
+        </button>
+        <button className="rounded-lg hover:bg-gray-100 w-full flex px-2 h-12 justify-start items-center space-x-2">
+          <RiNewsLine className="h-6 w-6" /> <span>NewsLetter</span>
         </button>
       </div>
+      <div className="w-full px-3">
+        <div className="border p-3 rounded-lg mb-4">
+          <h2 className="text-lg font-bold py-2">Upgrade to pro</h2>
+          <p className="pb-2 text-sm">
+            Unlock all courses, get access to source code, and more.
+          </p>
+          <button className="bg-green-950 text-white w-full rounded-lg py-2 hover:bg-green-900">
+            Upgrade
+          </button>
+        </div>
+      </div>
       <Separator className="my-4 " />
-      <div className="flex space-x-4 text-xs justify-between items-center w-full h-14">
+      <div className="flex space-x-4 text-xs justify-between items-center w-full px-3">
         <span>&#169; 2024 Lupleg Dev </span>
-        <button className="hover:bg-gray-100 p-2 rounded-md"><CiMenuKebab className="h-6 w-6" /></button>
+        <button className="hover:bg-gray-100 p-2 rounded-md">
+          <CiMenuKebab className="h-6 w-6" />
+        </button>
       </div>
     </div>
   );
