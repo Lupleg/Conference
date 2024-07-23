08784c0ea8f1445fde628ff6a7fa434884c701af
AngelaSinonge12-ux <angiesino361@gmail.com>
2024-07-22 23:39:26 -0700

    added desktop responsiveness.

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
-import  CardSection from "@/components/CardSection";
+import CardSection from "@/components/CardSection";
 export default function Home() {
- 
   return (
-    <div className="">
+    <div className="flex flex-col sm:flex-row px-4 sm:px-6 lg:px-20 py-8 sm:py-12 lg:py-20">
       <h2>Welcome to Course UI</h2>
-      <CardSection/>
+      <CardSection />
     </div>
   );
 }
