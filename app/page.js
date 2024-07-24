d9a2225dc8500c526bcaa2a6776d1fa9d211ae5a
Joshua Sabondo <sabondojoshua8@gmail.com>
2024-07-24 11:41:23 +0200

    card

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ import Card from "./Card";
 
 function Home() {
   return (
-    <div className="p-20">
-      <h2></h2>
+    <div className="flex flex-col sm:flex-row px-4 sm:px-6 lg:px-20 py-8 sm:py-12 lg:py-20 ">
+      <h2> Welcome to UI Course</h2>
 
       <Card />
     </div>
