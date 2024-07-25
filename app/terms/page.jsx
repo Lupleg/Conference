dae12080305c974e085f51d786eebb0e80e1418d
AngelaSinonge12-ux <angiesino361@gmail.com>
2024-07-24 18:27:34 -0700

    Created terms page.

---
+++ app/terms/page.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+
+export default function termspage() {
+  return (
+    <div className=" p-6 max-w-2xl mx-auto mt-8">
+      <h1 className="text-5xl font-bold text-orange-500 mb-4">
+        Terms and Conditions
+      </h1>
+      <p className="text-gray-700 mb-4 text-lg">
+        These Terms and Conditions ("Terms ", "Terms and Conditions ") govern
+        your relationship with Lupleg (the "Service ") operated by
+        <span className="text-green-900 font-bold"> Lupleg</span> ("us ", "we ",
+        or "our ").
+      </p>
+      <h3 className="text-xl font-bold mt-4 mb-2">1. Introduction</h3>
+      <p className="text-gray-700 mb-4 text-lg">
+        By using lupleg.website you confirm your acceptance of, and agree to be
+        bound by, these term and conditions.
+      </p>
+      <h3 className="text-xl font-bold mt-4 mb-2">
+        2.Agreement to Terms and Conditions
+      </h3>
+      <p className="text-gray-700 mb-4 text-lg">
+        This Agreement takes effect on the date on which you first use the
+        lupleg.website application.
+      </p>
+      <h3 className=" text-xl font-bold mt-4 mb-2">
+        3. PRO / premium features
+      </h3>
+      <p className="text-gray-700 mb-4 text-lg">
+        You will be able to use all (or some of) the PRO features after you
+        purchase a PRO tier. The details of the PRO features are available on
+        the pro page.
+      </p>
+      <h3 className="text-xl font-bold mt-4 mb-2">4. Refund Policy</h3>
+      <p className="text-gray-700 mb-4 text-lg">
+        We allow refunds within 14 days of purchase. Contact us at <br />
+        <b>info@lupleg.website</b> or in our Discord channel and we will give
+        you a refund.
+      </p>
+      <h3 className="text-xl font-bold mt-4 mb-2"> 5. Product usage</h3>
+      <p className="text-gray-700 mb-4 text-lg">
+        By using lupleg.website, you agree to receive important product updates,
+        newsletters, marketing or promotional materials and other information
+        that may be of interest to you. You may opt out of receiving any, or
+        all, of these communications from us by following the unsubscribe link
+        or instructions provided in any email we send or by contacting us.
+      </p>
+
+      <h3 className="text-xl font-bold mt-4 mb-2">6.Disclaimer</h3>
+      <p className="text-gray-700 mb-4 text-lg">
+        It is not warranted that Lupleg will meet your requirements or that its
+        operation will be uninterrupted or error free. All express and implied
+        warranties or conditions not stated in this Agreement (including without
+        limitation, loss of profits, loss or corruption of data, business
+        interruption or loss of contracts), so far as such exclusion or
+        disclaimer is permitted under the applicable law are excluded and
+        expressly disclaimed. This Agreement does not affect your statutory
+        rights.
+      </p>
+      <h3 className="text-xl font-bold mt-4 mb-2">
+        7.Warranties and Limitation of Liability
+      </h3>
+      <p className="text-gray-700 mb-4 text-lg">
+        lupleg.website does not give any warranty, guarantee or other term as to
+        the quality, fitness for purpose or otherwise of the software.
+        lupleg.website shall not be liable to you by reason of any
+        representation (unless fraudulent), or any implied warranty, condition
+        or other term, or any duty at common law, for any loss of profit or any
+        indirect, special or consequential loss, damage, costs, expenses or
+        other claims (whether caused by iCodeThis 's negligence or the
+        negligence of its servants or agents or otherwise) which arise out of or
+        in connection with the provision of any goods or services by
+        lupleg.website. lupleg.website shall not be liable or deemed to be in
+        breach of contract by reason of any delay in performing, or failure to
+        perform, any of its obligations if the delay or failure was due to any
+        cause beyond its reasonable control. Notwithstanding contrary clauses in
+        this Agreement, in the event that lupleg.website are deemed liable to
+        you for breach of this Agreement, you agree that lupleg.website 's
+        liability is limited to the amount actually paid by you for your
+        services or software, which amount calculated in reliance upon this
+        clause. You hereby release lupleg.website from any and all obligations,
+        liabilities and claims in excess of this limitation.
+      </p>
+      <h3 className="text-xl font-bold mt-4 mb-2">8. Changes</h3>
+      <p className="text-gray-700 mb-4 text-lg">
+        We reserve the right, at our sole discretion, to modify or replace these
+        Terms at any time. If a revision is material we will try to provide at
+        least 30 days notice prior to any new terms taking effect. What
+        constitutes a material change will be determined at our sole discretion.
+        By continuing to access or use our Service after those revisions become
+        effective, you agree to be bound by the revised terms. If you do not
+        agree to the new terms, please stop using the Service.
+      </p>
+      <h3 className="text-xl font-bold mt-4 mb-2">9. Governing Law</h3>
+      <p className="text-gray-700 mb-4 text-lg ">
+        These Terms shall be governed and construed in accordance with the laws
+        of Zambia (ZM), without regard to its conflict of law provisions. Our
+        failure to enforce any right or provision of these Terms will not be
+        considered a waiver of those rights. If any provision of these Terms is
+        held to be invalid or unenforceable by a court, thediving provisions of
+        these Terms will remain in effect. These Terms constitute the entire
+        agreement between us regarding our Service, and supersede and replace
+        any prior agreements we might have between us regarding the Service.
+        <span className="font-bold text-black"> Last updated:</span> 04 June
+        2024.
+      </p>
+    </div>
+  );
+}
