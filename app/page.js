424dcd7fc180f5cd790ab28b9f8495268f5b20bc
Joshua Sabondo <sabondojoshua8@gmail.com>
2024-07-25 09:42:29 +0200

    card

--- app/page.js
+++ app/page.js
@@ -1,14 +1,16 @@
-import React from "react";
-import Card from "./Card";
+import React from "react"
+
+import Card from "@/components/ui/card";
 
 
 
 function Home() {
   return (
-    <div className="flex flex-col sm:flex-row px-4 sm:px-6 lg:px-20 py-8 sm:py-12 lg:py-20 ">
+    <div className="flex flex-col sm:flex-row px-2 sm:px-2 lg:px-20 py-8 sm:py-12 lg:px-18 ">
       <h2> Welcome to UI Course</h2>
-
-      <Card />
+      
+       <Card/>;
+      
     </div>
   );
 }
--- components/ui/card.jsx
+++ components/ui/card.jsx
@@ -5,24 +5,24 @@ import { LuFilePlus2 } from "react-icons/lu";
 
 function Card() {
   return (
-    <div className="container p-4 mx-auto  flex flex-col sm:flex-row items-center justify-center min-h-screen p-4">
+    <div className="container p-2 mx-auto  flex flex-col sm:flex-row items-center justify-center min-h-screen p-2">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
         <div className="bg-white shadow-lg p-4 border border-gray-200 rounded-3xl max-w-xs h-full hover:shadow-lg hover:scale-110 transition duration-300 ease-out overflow-hidden">
           <div className="h-60 -mx-4 -mt-4 relative">
             <img
               src="/study.jpg"
               alt=""
-              className="w-full h-full object-cover"
+              className="w-full h-48 object-cover"
             />
           </div>
-          <h2 className="text-blue-400 mt-4 text-2xl flex items-center mx-4">
+          <h2 className="text-blue-600 mt-2 text-2xl flex justify-content">
             <LuFilePlus2 className="mr-2" /> FRONT-END
           </h2>
-          <p className="w-full text-sm-2xl text-lg mb-2 font-bold">
-            Beginner's Guide to Becoming a Professional Developer Expert
+          <p className=" w-full text-sm-2xl text-lg mb-2 font-bold">
+            Beginner's Guide to Becoming a Professional Developer
           </p>
           <div className="flex justify-start mb-4">
-            <hr className="w-1/2 border-none h-1 bg-green-600" />
+            <hr className="w-full border-none h-1 bg-green-600" />
           </div>
 
           <div className="flex items-center mt-4">
@@ -43,17 +43,17 @@ function Card() {
             <img
               src="/studyy.jpg"
               alt=""
-              className="w-full h-full object-cover"
+              className="w-full h-48 object-cover"
             />
           </div>
-          <h2 className="text-purple-500 mt-4 text-2xl flex items-center mx-4">
+          <h2 className="text-purple-600 mt-2 text-2xl flex items-center">
             <GiPencilRuler className="mr-2" /> UI/UX DESIGN
           </h2>
-          <p className="w-full text-sm-2xl text-lg mb-2 font-bold">
+          <p className=" w-full text-sm-2xl text-lg mb-2 font-bold">
             Optimising User Experience with the Best UI/UX Design
           </p>
           <div className="flex justify-start mb-4">
-            <hr className="w-1/2 border-none h-1 bg-blue-600" />
+            <hr className="w-full border-none h-1 bg-blue-600" />
           </div>
 
           <div className="flex items-center mt-4">
@@ -74,17 +74,17 @@ function Card() {
             <img
               src="/computer.jpg"
               alt=""
-              className="w-full h-full object-cover"
+              className="w-full h-48 object-cover"
             />
           </div>
-          <h2 className="text-green-500 mt-4 text-2xl flex items-center mx-4">
+          <h2 className="text-green-500 mt-2 text-2xl flex items-center">
             <MdBrandingWatermark className="mr-2" /> BRANDING
           </h2>
-          <p className="w-full text-sm-2xl text-lg mb-2 font-bold">
+          <p className="  w-full text-sm-2xl text-lg mb-2 font-bold">
             Reviving and Refresh Company Image
           </p>
           <div className="flex justify-start mb-4 ml-auto">
-            <hr className="w-1/2 border-none h-1 bg-purple-600" />
+            <hr className="w-full border-none h-1 bg-purple-600" />
           </div>
 
           <div className="flex items-center mt-4">
