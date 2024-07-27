7b3fa1fdb431b19df288592f65bc1d2557bb6870
Joshua Sabondo <sabondojoshua8@gmail.com>
2024-07-27 22:29:46 +0200

    card

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className=" flex flex-wrap px-4 sm:px-6 py-8 sm:py-12 lg:py-20 w-full">
       <h2> Welcome to UI Course</h2>
-      <div className="flex flex-wrap w-full bg-white gap-4 justify-around">
+      <div className=" px-2 flex flexbox flex-wrap w-full bg-white gap-5 justify-around">
         <Card />
         <Card />
         <Card />
--- components/ui/card.jsx
+++ components/ui/card.jsx
@@ -5,26 +5,29 @@ import { LuFilePlus2 } from "react-icons/lu";
 
 function Card() {
   return (
-    <div className="shadow-lg sm:flex sm:flex-col bg-white rounded-3xl overflow-hidden sm:w-70 border border-gray-200  mx-auto  ">
+    <div className="shadow-lg bg-white rounded-3xl overflow-hidden sm:w-80 md:w-96 lg:w-1/4 border border-gray-200 mx-auto min-w-[400px] hover:shadow-lg hover:scale-110 transition duration-300 ease-out overflow-hidden ">
       <div>
-        <div className=" mt-[-2] flex-box h-60 -mx-4 rounded-t-3xl relative  ">
-          <img src="/studyy.jpg" alt="" className="w-full h-full object-cover" />
+        <div className=" mt-[-2] flexbox h-60 -mx-4 rounded-t-3xl relative  ">
+          <img
+            src="/studyy.jpg"
+            alt=""
+            className="w-full h-full object-cover"
+          />
         </div>
       </div>
-      <div className="p-2 text-green-600 flex  text-2xl my-1 font-bold">
+      <div className="p-3 text-green-600 flex  text-2xl my-1 font-bold">
         <LuFilePlus2 className="mr-2" />
         <h2>FRONT-END</h2>
       </div>
-      <p className="px-2 w-full text-lg-2xl mb-2  font-bold text-gray-800 break-words ">
-        Beginner's Guide to Becoming a Professional <br />
-        Front-end Developer
+      <p className="px-4 w-full text-lg mb-2  font-bold text-gray-800  line-clamp-2 break-words ">
+        Beginner's Guide to Becoming a Professional Front-end Developer
       </p>
 
-      <div className="flex justify-start mb-4 px-4">
+      <div className="flex justify-start mb-4 px-5">
         <hr className="w-full border-none h-1 bg-green-600" />
       </div>
 
-      <div className="flex items-center  ">
+      <div className="p-2 flex items-center  ">
         <img src="/avator.jpg" alt="" className="w-12 h-12 rounded-full mr-4" />
         <div>
           <p className="text-sm text-black-600 font-bold">Joshua Samsul</p>
